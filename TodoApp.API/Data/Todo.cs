7fb425bc9093c82bc6f67a33b4d1eea526af384e
Evgeni Bekker <masterdemilich@gmail.com>
2022-01-22 18:02:14 +0900

    Add project files.

---
+++ TodoApp.API/Data/Todo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoApp.API.Data
+{
+    public partial class Todo
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ TodoApp.API/Data/TodoDBContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace TodoApp.API.Data
+{
+    public partial class TodoDBContext : DbContext
+    {
+        public TodoDBContext()
+        {
+        }
+
+        public TodoDBContext(DbContextOptions<TodoDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Todo> Todos { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Todo>(entity =>
+            {
+                entity.Property(e => e.Description)
+                    .HasMaxLength(500)
+                    .HasColumnName("description");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .HasColumnName("name");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
